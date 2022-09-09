9be7b05deaf0d1b40d3a03b82386e59c84ceb5cc
Jun Kimura <jun.kimura@datachain.jp>
2022-09-09 18:40:16 +0900

    relay: enable tendermint module
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- go/relay/bin/main.go
+++ go/relay/bin/main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 
 	lcp "github.com/datachainlab/lcp/go/relay"
+	tendermint "github.com/hyperledger-labs/yui-relayer/chains/tendermint/module"
 	"github.com/hyperledger-labs/yui-relayer/cmd"
 )
 
 func main() {
 	if err := cmd.Execute(
 		lcp.Module{},
+		tendermint.Module{},
 	); err != nil {
 		log.Fatal(err)
 	}
