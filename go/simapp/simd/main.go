f7e40e707db2b597b6dc4e35ca412e5f07f96d55
Jun Kimura <jun.kimura@datachain.jp>
2022-09-11 23:47:17 +0900

    simapp: enable debug flag for AVR verification
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- go/simapp/simd/main.go
+++ go/simapp/simd/main.go
@@ -5,13 +5,18 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	"github.com/datachainlab/lcp/go/sgx/ias"
 	"github.com/datachainlab/lcp/go/simapp"
 	"github.com/datachainlab/lcp/go/simapp/simd/cmd"
 )
 
 func main() {
-	rootCmd, _ := cmd.NewRootCmd()
+	// WARNING: if you use the simd in production, you must remove the following code:
+	ias.SetAllowDebugEnclaves()
+	defer ias.UnsetAllowDebugEnclaves()
+	// END WARNING
 
+	rootCmd, _ := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, simapp.DefaultNodeHome); err != nil {
 		switch e := err.(type) {
 		case server.ErrorCode:
