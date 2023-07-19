b65668a728bbeca5c7630bfa4b08c8fe1b2bbfb3
Jun Kimura <jun.kimura@datachain.jp>
2023-07-19 19:07:49 +0900

    app: use git tag as version
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- app/build.rs
+++ app/build.rs
@@ -1,9 +1,13 @@
+use git2::{DescribeOptions, Repository};
 use std::env;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let sdk_dir = env::var("SGX_SDK").unwrap_or_else(|_| "/opt/sgxsdk".to_string());
     let sgx_mode = env::var("SGX_MODE").unwrap_or_else(|_| "HW".to_string());
-
+    let mut opts = DescribeOptions::new();
+    opts.describe_tags().show_commit_oid_as_fallback(true);
+    let version = Repository::discover(".")?.describe(&opts)?.format(None)?;
+    println!("cargo:rustc-env=LCP_VERSION={}", version);
     println!("cargo:rustc-link-search=native=./lib");
     println!("cargo:rustc-link-lib=static=Enclave_u");
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
@@ -24,4 +28,5 @@ fn main() {
             println!("cargo:rustc-link-lib=dylib=sgx_uae_service");
         }
     }
+    Ok(())
 }
