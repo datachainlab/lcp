f5324530d8c978d749e84c8d1cb7636ab604ffe8
Jun Kimura <jun.kimura@datachain.jp>
2024-09-08 15:45:37 +0900

    remove unnecessary function
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- modules/crypto/src/sgx/rand.rs
+++ modules/crypto/src/sgx/rand.rs
@@ -1,17 +1,7 @@
 use crate::errors::Error;
-use sgx_rand::Rng;
 use sgx_trts::trts::rsgx_read_rand;
 use sgx_types::sgx_status_t;
 
 pub fn rand_slice(rand: &mut [u8]) -> Result<(), Error> {
     rsgx_read_rand(rand).map_err(Error::sgx_error)
-}
-
-pub fn fill_bytes(bytes: &mut [u8]) -> Result<(), sgx_status_t> {
-    let mut os_rng = match sgx_rand::SgxRng::new() {
-        Ok(r) => r,
-        Err(_) => return Err(sgx_status_t::SGX_ERROR_UNEXPECTED),
-    };
-    os_rng.fill_bytes(bytes);
-    Ok(())
 }
