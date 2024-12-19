b4a964b3a5f399300c7caa85d9df3894c45c764d
Jun Kimura <jun.kimura@datachain.jp>
2024-12-19 19:42:53 +0900

    fix field order of `DCAPVerifierCommit`
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- modules/lcp-client/src/dcap.rs
+++ modules/lcp-client/src/dcap.rs
@@ -2,9 +2,9 @@ use dcap_rs::{types::VerifiedOutput, utils::hash::keccak256sum};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct DCAPVerifierCommit {
-    pub output: VerifiedOutput,
     pub attestation_time: u64,
     pub sgx_intel_root_ca_hash: [u8; 32],
+    pub output: VerifiedOutput,
 }
 
 impl DCAPVerifierCommit {
@@ -18,23 +18,23 @@ impl DCAPVerifierCommit {
 
     pub fn to_bytes(&self) -> Vec<u8> {
         let mut output = self.output.to_bytes();
-        let mut aux: [u8; 40] = [0; 40];
-        aux[..8].copy_from_slice(&self.attestation_time.to_be_bytes());
-        aux[8..].copy_from_slice(&self.sgx_intel_root_ca_hash);
-        output.extend_from_slice(aux.as_ref());
-        output
+        let mut bytes = Vec::new();
+        bytes.extend_from_slice(&self.attestation_time.to_le_bytes());
+        bytes.extend_from_slice(&self.sgx_intel_root_ca_hash);
+        bytes.append(&mut output);
+        bytes
     }
 
     pub fn from_bytes(slice: &[u8]) -> Self {
-        let output = VerifiedOutput::from_bytes(&slice[..slice.len() - 48]);
         let mut attestation_time = [0; 8];
-        attestation_time.copy_from_slice(&slice[slice.len() - 48..slice.len() - 40]);
+        attestation_time.copy_from_slice(&slice[0..8]);
         let mut sgx_intel_root_ca_hash = [0; 32];
-        sgx_intel_root_ca_hash.copy_from_slice(&slice[slice.len() - 40..]);
+        sgx_intel_root_ca_hash.copy_from_slice(&slice[8..40]);
+        let output = VerifiedOutput::from_bytes(&slice[40..]);
         Self {
-            output,
-            attestation_time: u64::from_be_bytes(attestation_time),
+            attestation_time: u64::from_le_bytes(attestation_time),
             sgx_intel_root_ca_hash,
+            output,
         }
     }
 
