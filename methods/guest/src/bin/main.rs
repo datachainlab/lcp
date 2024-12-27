4b082931dda6346c940f5b09469f9ccbe209a867
Jun Kimura <jun.kimura@datachain.jp>
2024-12-27 16:50:09 +0900

    fix commit
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- methods/guest/src/bin/main.rs
+++ methods/guest/src/bin/main.rs
@@ -4,6 +4,7 @@ use dcap_rs::types::collaterals::IntelCollateral;
 use risc0_zkvm::guest::env;
 use dcap_rs::utils::quotes::version_3::verify_quote_dcapv3;
 use dcap_rs::types::quotes::version_3::QuoteV3;
+use dcap_rs::types::DCAPVerifierCommit;
 
 fn main() {
     let (quote, collaterals, current_time): (Vec<u8>, Vec<u8>, u64) = env::read();
@@ -12,5 +13,6 @@ fn main() {
     let collaterals = IntelCollateral::from_bytes(&collaterals);
 
     let output = verify_quote_dcapv3(&quote, &collaterals, current_time);
-    env::commit_slice(output.to_bytes().as_slice());
+    let commit = DCAPVerifierCommit::new(current_time, output, collaterals.sgx_intel_root_ca_der.unwrap().as_slice());
+    env::commit_slice(commit.to_bytes().as_slice());
 }
--- modules/attestation-report/src/dcap.rs
+++ modules/attestation-report/src/dcap.rs
@@ -107,56 +107,7 @@ impl ZKDCAPQuote {
     }
 
     #[cfg(feature = "std")]
-    pub fn commit(&self) -> DCAPVerifierCommit {
-        DCAPVerifierCommit::from_bytes(self.zkp.commit())
-    }
-}
-
-#[cfg(feature = "std")]
-#[derive(Debug, Clone, PartialEq)]
-pub struct DCAPVerifierCommit {
-    pub attestation_time: u64,
-    pub sgx_intel_root_ca_hash: [u8; 32],
-    pub output: dcap_rs::types::VerifiedOutput,
-}
-
-#[cfg(feature = "std")]
-impl DCAPVerifierCommit {
-    pub fn new(
-        attestation_time: u64,
-        output: dcap_rs::types::VerifiedOutput,
-        sgx_intel_root_ca: &[u8],
-    ) -> Self {
-        Self {
-            attestation_time,
-            sgx_intel_root_ca_hash: dcap_rs::utils::hash::keccak256sum(sgx_intel_root_ca),
-            output,
-        }
-    }
-
-    pub fn to_bytes(&self) -> Vec<u8> {
-        let mut output = self.output.to_bytes();
-        let mut bytes = Vec::new();
-        bytes.extend_from_slice(&self.attestation_time.to_le_bytes());
-        bytes.extend_from_slice(&self.sgx_intel_root_ca_hash);
-        bytes.append(&mut output);
-        bytes
-    }
-
-    pub fn from_bytes(slice: &[u8]) -> Self {
-        let mut attestation_time = [0; 8];
-        attestation_time.copy_from_slice(&slice[0..8]);
-        let mut sgx_intel_root_ca_hash = [0; 32];
-        sgx_intel_root_ca_hash.copy_from_slice(&slice[8..40]);
-        let output = dcap_rs::types::VerifiedOutput::from_bytes(&slice[40..]);
-        Self {
-            attestation_time: u64::from_le_bytes(attestation_time),
-            sgx_intel_root_ca_hash,
-            output,
-        }
-    }
-
-    pub fn hash(&self) -> [u8; 32] {
-        dcap_rs::utils::hash::keccak256sum(&self.to_bytes())
+    pub fn commit(&self) -> dcap_rs::types::DCAPVerifierCommit {
+        dcap_rs::types::DCAPVerifierCommit::from_bytes(self.zkp.commit())
     }
 }
