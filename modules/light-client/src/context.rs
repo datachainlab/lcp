48448fd3d54b991afa091db11185c7362f4756ce
Kiyoshi Nakao <kiyoshi.nakao@datachain.jp>
2026-05-12 15:23:06 +0900

    types: centralize light client store keys

--- modules/light-client/src/context.rs
+++ modules/light-client/src/context.rs
@@ -1,9 +1,6 @@
 use crate::types::{Any, ClientId, Height, Time};
-use crate::{
-    errors::Error,
-    path::{ClientConsensusStatePath, ClientStatePath, ClientTypePath},
-    prelude::*,
-};
+use crate::{errors::Error, prelude::*};
+use lcp_types::store_key;
 use store::KVStore;
 
 pub trait HostContext {
@@ -14,13 +11,13 @@ pub trait HostContext {
 pub trait ClientReader: KVStore {
     /// Returns `true` if the client exists in the store.
     fn client_exists(&self, client_id: &ClientId) -> bool {
-        self.get(format!("{}", ClientTypePath::new(client_id)).as_bytes())
+        self.get(store_key::client_type(client_id.as_str()).as_bytes())
             .is_some()
     }
 
     /// Returns the ClientType for the given identifier `client_id`.
     fn client_type(&self, client_id: &ClientId) -> Result<String, Error> {
-        let value = self.get(format!("{}", ClientTypePath::new(client_id)).as_bytes());
+        let value = self.get(store_key::client_type(client_id.as_str()).as_bytes());
         if let Some(value) = value {
             Ok(String::from_utf8(value).unwrap())
         } else {
@@ -30,7 +27,7 @@ pub trait ClientReader: KVStore {
 
     /// Returns the ClientState for the given identifier `client_id`.
     fn client_state(&self, client_id: &ClientId) -> Result<Any, Error> {
-        let value = self.get(format!("{}", ClientStatePath::new(client_id)).as_bytes());
+        let value = self.get(store_key::client_state(client_id.as_str()).as_bytes());
         if let Some(value) = value {
             Ok(
                 bincode::serde::decode_from_slice(&value, bincode::config::standard())
@@ -47,8 +44,8 @@ pub trait ClientReader: KVStore {
     ///
     /// Returns an error if no such state exists.
     fn consensus_state(&self, client_id: &ClientId, height: &Height) -> Result<Any, Error> {
-        let path = ClientConsensusStatePath::new(client_id, height);
-        let value = match self.get(format!("{}", path).as_bytes()) {
+        let key = store_key::consensus_state(client_id.as_str(), height);
+        let value = match self.get(key.as_bytes()) {
             Some(value) => value,
             None => {
                 return Err(Error::consensus_state_not_found(client_id.clone(), *height));
@@ -66,7 +63,7 @@ pub trait ClientKeeper: ClientReader {
     /// Called upon successful client creation
     fn store_client_type(&mut self, client_id: ClientId, client_type: String) -> Result<(), Error> {
         self.set(
-            format!("{}", ClientTypePath(client_id)).into_bytes(),
+            store_key::client_type_bytes(client_id.as_str()),
             client_type.into_bytes(),
         );
         Ok(())
@@ -79,10 +76,7 @@ pub trait ClientKeeper: ClientReader {
         client_state: Any,
     ) -> Result<(), Error> {
         let bz = bincode::serde::encode_to_vec(&client_state, bincode::config::standard()).unwrap();
-        self.set(
-            format!("{}", ClientStatePath::new(&client_id)).into_bytes(),
-            bz,
-        );
+        self.set(store_key::client_state_bytes(client_id.as_str()), bz);
         Ok(())
     }
 
@@ -95,8 +89,10 @@ pub trait ClientKeeper: ClientReader {
     ) -> Result<(), Error> {
         let bz =
             bincode::serde::encode_to_vec(&consensus_state, bincode::config::standard()).unwrap();
-        let path = ClientConsensusStatePath::new(&client_id, &height);
-        self.set(format!("{}", path).into_bytes(), bz);
+        self.set(
+            store_key::consensus_state_bytes(client_id.as_str(), &height),
+            bz,
+        );
         Ok(())
     }
 }
