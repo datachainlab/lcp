bfe621d4514d04af297f9a50caf64a27d0b2ff85
Kiyoshi Nakao <kiyoshi.nakao@datachain.jp>
2026-06-08 10:50:50 +0900

    service: store light-client state id on init

--- enclave-modules/ecall-handler/src/light_client/init_client.rs
+++ enclave-modules/ecall-handler/src/light_client/init_client.rs
@@ -5,8 +5,8 @@ use core::str::FromStr;
 use crypto::Signer;
 use ecall_commands::{InitClientInput, InitClientResponse, LightClientResponse};
 use lcp_types::ClientId;
-use light_client::commitments::{prove_commitment, CommitmentProof};
-use light_client::{commitments::gen_state_id_from_any, ClientKeeper, ClientReader, LightClientResolver};
+use light_client::commitments::{prove_commitment, CommitmentProof, ProxyMessage};
+use light_client::{ClientKeeper, ClientReader, LightClientResolver};
 use store::KVStore;
 
 pub fn init_client<R: LightClientResolver, S: KVStore, K: Signer>(
@@ -34,7 +34,15 @@ pub fn init_client<R: LightClientResolver, S: KVStore, K: Signer>(
     if ctx.client_exists(&client_id) {
         return Err(Error::client_already_exists(client_id.to_string()));
     }
-    let state_id = gen_state_id_from_any(&input.any_client_state, &input.any_consensus_state)?;
+    let state_id = match &res.message {
+        ProxyMessage::UpdateState(message) => message.post_state_id,
+        message => {
+            return Err(Error::invalid_argument(format!(
+                "create_client must return update-state message: actual_type={}",
+                message.message_type()
+            )))
+        }
+    };
     ctx.store_client_type(client_id.clone(), client_type)?;
     ctx.store_any_client_state(client_id.clone(), input.any_client_state)?;
     ctx.store_any_consensus_state(client_id.clone(), res.height, input.any_consensus_state)?;
